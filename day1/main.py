cffcb82477b6f879f2ad717311823d389cf55b0e
Faisal <md.faisal1899@yahoo.com>
2022-03-04 19:30:43 +0600

    fixed some code- print multiple line

---
+++ day1/main.py
@@ -0,0 +1,5 @@
+# first python code
+print("hello world")
+
+print("Day 1 - String Manipulation\n"+"String Concatenation is done with the '+' sign.\n" + 'e.g. print("Hello " + "world")\n' + "New lines can be created with a backslash and n.")
+
