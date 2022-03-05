75f7f5d3d84a8b6412a19c1a0311601f907474c3
Faisal <md.faisal1899@yahoo.com>
2022-03-05 06:10:04 +0600

    completed day 1 - print,length of string, variable, line break, concat etc

--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,51 @@
+##
 # first python code
-print("hello world")
+#print("hello world")
 
-print("Day 1 - String Manipulation\n"+"String Concatenation is done with the '+' sign.\n" + 'e.g. print("Hello " + "world")\n' + "New lines can be created with a backslash and n.")
+##
+#writing multiple line into print function
+# print("Day 1 - String Manipulation\n"+"String Concatenation is done with the '+' sign.\n" + 'e.g. print("Hello " + "world")\n' + "New lines can be created with a backslash and n.")
+
+##
+# when full function start executing , it will move to input function, catch the value , then print the string 'hello name'
+#print("hello " + input("what is your name?"))
+
+##
+#print the length of the inputed string
+#print( len( input("What is your name?") ) )
+
+
+##variable
+# name = input("What is your name?")
+# length = len(name)
+# print(length) 
+
+#Write a program that switches the values stored in the variables a and b.
+# # 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# # 🚨 Don't change the code above 👆
+
+# ####################################
+# #Write your code below this line 👇
+# c = a
+# a = b
+# b = c
+
+
+
+# #Write your code above this line 👆
+# ####################################
+
+# # 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+
+##
+# Band name generator
+print("Welcome to Band Name Generator")
+city = input("In which city did you grow up?\n")
+petName = input("What is your favourite pet's name\n")
+print("Your band name is: " + city + " " + petName)
 
