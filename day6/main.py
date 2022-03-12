2e001017d05b8d8b00b5b7427a2d64f4711e5089
Faisal <md.faisal1899@yahoo.com>
2022-03-12 09:47:08 +0600

    completed all hurdles problems

---
+++ day6/main.py
@@ -0,0 +1,27 @@
+## function, while loop
+
+## by this piece code, it can solve all hurdles problem(1-4) of the following link
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     while not right_is_clear():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
