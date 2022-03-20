322eb41983c3d6d8f668bd6c3d93822775ce7453
Faisal <md.faisal1899@yahoo.com>
2022-03-20 09:18:41 +0600

    some more basic

---
+++ basic2/fundamental/basic.py
@@ -0,0 +1,13 @@
+import os
+
+os.system("cls")
+
+name = "faisal"
+
+print(len(name))
+print(name[0:3])  # extract first three letter.
+print(name[0:])
+print(name[:])
+print(name[:3])
+
+# arguments, *argu, **argu, fizzbuzz
